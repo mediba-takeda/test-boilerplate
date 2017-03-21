8be88e433852b452fa0edcdf8a06f6426e2f25a6
Satoshi Takeda <takeda@mediba.jp>
2017-03-21 10:01:05 +0900

    add test case

---
+++ test/users/UsersView.spec.js
@@ -0,0 +1,13 @@
+import $ from 'jquery'
+import UsersView from 'users/UsersView'
+import assert from 'power-assert'
+
+describe('UsersView のメソッドをテストする', () => {
+  it('指定のセレクタにテキストをアペンドすることができる', () => {
+    const string = 'dummy text'
+    $('body').append('<div class="root" />')
+    UsersView.append(string)
+    const appendStr = $('.root').text()
+    assert(string === appendStr)
+  })
+})
