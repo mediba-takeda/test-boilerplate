59a69c5e6b0db2fcaa3d0c7055afae030d455de3
Satoshi Takeda <tkdnation@gmail.com>
2017-06-28 01:06:37 +0900

    add fakeserver test

---
+++ test/users/UserRequest.spec.js
@@ -0,0 +1,35 @@
+import $ from 'jquery'
+import UserRequest from 'users/UsersRequest'
+
+describe('- UserRequest', () => {
+  let server
+
+  beforeEach(() => {
+    server = sinon.fakeServer.create()
+  })
+
+  afterEach(() => {
+    server.restore()
+    server = null
+  })
+
+  it('UserRequest#fetcth - Promise を返却してチェーンできる', (done) => {
+    const request = new UserRequest
+    server.respondWith(
+      'GET',
+      'http://jsonplaceholder.typicode.com/users',
+      [
+        200,
+        { 'Content-Type': 'application/json' },
+        JSON.stringify({status: 1})
+      ]
+    )
+    request.fetch()
+      .then( res => {
+        assert.deepEqual(res, {status: 1})
+        done()
+      })
+    server.respond()
+  })
+
+})
--- test/users/UsersView.spec.js
+++ test/users/UsersView.spec.js
@@ -3,10 +3,10 @@ import UsersView from 'users/UsersView'
 
 describe('- UsersView', () => {
 
-  it('UsersView#append - ', () => {
+  it('UsersView#append - 文字列の引数を受けて指定の要素にインジェクトできる', () => {
     const expected = 'dummy text'
     $('body').append('<div class="root" />')
-    UsersView.append(string)
+    UsersView.append(expected)
     const actual = $('.root').text()
     assert(expected === actual)
   })
